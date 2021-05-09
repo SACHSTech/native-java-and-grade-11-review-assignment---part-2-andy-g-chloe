3c96dcf052cbd6e001e72c7ae981aec5b8301675
Andy Guo <>
2021-05-09 18:55:02 +0000

    Cleaned up files

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,19 +1,1 @@
 package gr11review.part2;
-
-/* Compiling and running:
-
-javac -d bin src/gr11review/part2/Utility.java
-javac -d bin src/gr11review/part2/Main.java
-java -classpath bin gr11review.part2.Main
-java -classpath bin gr11review.part2.Utility
-
-*/
-
-public class Main {
-  public static void main(String[] args) {
-    int myInt;
-    myInt = Utility.sumNumbers("abc123abc");
-    System.out.println(myInt);
-  }
-
-}
